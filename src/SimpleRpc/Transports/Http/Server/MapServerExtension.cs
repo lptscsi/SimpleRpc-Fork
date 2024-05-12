74440587611be477dd80f0d8bdb7c4af2f03de92
Maxim T <BBGONE@users.noreply.github.com>
2024-05-12 20:37:50 +0300

    Update MapServerExtension.cs

--- src/SimpleRpc/Transports/Http/Server/MapServerExtension.cs
+++ src/SimpleRpc/Transports/Http/Server/MapServerExtension.cs
@@ -7,6 +7,7 @@ using System;
 using System.Diagnostics.CodeAnalysis;
 using System.IO;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
 
 namespace SimpleRpc.Transports.Http.Server
 {
@@ -15,48 +16,52 @@ namespace SimpleRpc.Transports.Http.Server
         public static RouteHandlerBuilder MapRpcServer<TService>(this IEndpointRouteBuilder app, [StringSyntax("Route")] string pattern)
             where TService : class
         {
-            return app.MapPost(pattern, async Task<Results<FileContentHttpResult, FileStreamHttpResult, BadRequest<string>>> (RpcRequest request, RpcServer<TService> rpcServer) =>
-            {
-                var serializer = SerializationHelper.Json;
-                try
-                {
-                    object rpcResponse = await rpcServer.Invoke(request);
+            var value = async Task<Results<FileContentHttpResult, FileStreamHttpResult, BadRequest<string>>> (RpcRequest request, RpcServer<TService> rpcServer, ILogger<RpcServer<TService>> logger) =>
+                        {
+                            var serializer = SerializationHelper.Json;
+                            try
+                            {
+                                object rpcResponse = await rpcServer.Invoke(request);
 
-                    if (rpcResponse is RpcResponse response)
-                    {
-                        MemoryStream memoryStream = new MemoryStream();
-                        await serializer.Serialize(response, memoryStream);
-                        return TypedResults.Bytes(memoryStream.ToArray(), serializer.ContentType);
-                    }
-                    else if (rpcResponse is Stream stream)
-                    {
-                        return TypedResults.Stream(stream, System.Net.Mime.MediaTypeNames.Application.Octet);
-                    }
-                    else
-                    {
-                        return TypedResults.BadRequest($"Unexpected response type: {rpcResponse?.GetType().Name ?? "null"}");
-                    }
-                }
-                catch (Exception e)
-                {
-                    RpcError rpcError = new RpcError
-                    {
-                        Code = RpcErrorCode.IncorrectRequestBodyFormat,
-                        Exception = e.Message,
-                    };
+                                if (rpcResponse is RpcResponse response)
+                                {
+                                    MemoryStream memoryStream = new MemoryStream();
+                                    await serializer.Serialize(response, memoryStream);
+                                    return TypedResults.Bytes(memoryStream.ToArray(), serializer.ContentType);
+                                }
+                                else if (rpcResponse is Stream stream)
+                                {
+                                    return TypedResults.Stream(stream, System.Net.Mime.MediaTypeNames.Application.Octet);
+                                }
+                                else
+                                {
+                                    return TypedResults.BadRequest($"Unexpected response type: {rpcResponse?.GetType().Name ?? "null"}");
+                                }
+                            }
+                            catch (Exception e)
+                            {
+                                logger.LogError(e, "SimpleRpc server unexpected error");
 
-                    //  _logger.LogError(e, rpcError.Code.ToString());
+                                RpcError rpcError = new RpcError
+                                {
+                                    Code = RpcErrorCode.IncorrectRequestBodyFormat,
+                                    Exception = e.Message,
+                                };
 
-                    RpcResponse rpcResponse = new RpcResponse()
-                    {
-                        Error = rpcError,
-                        Result = null
-                    };
-                    MemoryStream memoryStream = new MemoryStream();
-                    await serializer.Serialize(rpcResponse, memoryStream);
-                    return TypedResults.Bytes(memoryStream.ToArray(), serializer.ContentType);
-                }
-            });
+                                //  _logger.LogError(e, rpcError.Code.ToString());
+
+                                RpcResponse rpcResponse = new RpcResponse()
+                                {
+                                    Error = rpcError,
+                                    Result = null
+                                };
+                                MemoryStream memoryStream = new MemoryStream();
+                                await serializer.Serialize(rpcResponse, memoryStream);
+                                return TypedResults.Bytes(memoryStream.ToArray(), serializer.ContentType);
+                            }
+                        };
+
+            return app.MapPost(pattern, value);
         }
     }
 }
